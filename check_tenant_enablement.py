2ae5e2e2ff9147bcc3f40cf784048c90e42368cb
hseccx <hseccx@proton.me>
2025-03-24 08:22:42 -0500

    Consolidated files for ease of use

--- check_tenant_enablement.py
+++ check_tenant_enablement.py
@@ -2,7 +2,33 @@ import concurrent.futures
 import logging
 import argparse
 from pathlib import Path
-from utils.test_tenant_enabled import generate_oauth_token_test, load_tenants
+import requests
+
+
+def load_tenants(tenants_file=Path("tenants.txt").resolve()) -> list:
+    with open(tenants_file, "r") as file:
+        return list(dict.fromkeys(file.read().splitlines()))
+    
+def generate_oauth_token_test(iam_url: str, tenant: str) -> str:
+    """
+    Generates an OAuth token using the provided API key.
+
+    Parameters:
+        config (dict): Configuration dictionary with IAM URL, API key, and tenant name.
+
+    Returns:
+        str: The OAuth token if successful, or an error message if not.
+    """
+    url = f"{iam_url}/auth/realms/{tenant}/protocol/openid-connect/token"
+    data = {
+        "grant_type": "refresh_token",
+        "client_id": "ast-app",
+        "refresh_token": " "
+    }
+    response = requests.post(url, data=data)
+    if response.status_code == 200:
+        return response.json().get("access_token", "Error: Access token not found.")
+    return f"Error: Failed to generate token. Status: {response.status_code}, Response: {response.text}"
 
 # Configure logging format and level
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
